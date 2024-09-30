98ac610552d4802603c42b7e8d752c5e9119ead4
Nguyen Vi Tien <Nguyevitien2003@gmail.com>
2024-09-30 07:58:12 +0700

    [Fix] Fix db

---
+++ FA24_SE1716_PRN231_G1_KOIFARMSHOP/KOIFARMSHOP.Data/Models/OpalExeContext.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace KOIFARMSHOP.Data.Models;
+
+public partial class OpalExeContext : DbContext
+{
+    public OpalExeContext(DbContextOptions<OpalExeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Animal> Animals { get; set; }
+
+    public virtual DbSet<AnimalImage> AnimalImages { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Consignment> Consignments { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+    public virtual DbSet<OrderPromotion> OrderPromotions { get; set; }
+
+    public virtual DbSet<Payment> Payments { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<ProductImage> ProductImages { get; set; }
+
+    public virtual DbSet<Promotion> Promotions { get; set; }
+
+    public virtual DbSet<Staff> Staff { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Animal>(entity =>
+        {
+            entity.HasKey(e => e.AnimalId).HasName("PK__Animals__A21A73271E85A11C");
+
+            entity.Property(e => e.AnimalId).HasColumnName("AnimalID");
+            entity.Property(e => e.AmountFeed).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Certificate).HasMaxLength(255);
+            entity.Property(e => e.Color).HasMaxLength(50);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.FarmOrigin).HasMaxLength(100);
+            entity.Property(e => e.Gender).HasMaxLength(10);
+            entity.Property(e => e.HealthStatus).HasMaxLength(255);
+            entity.Property(e => e.MaintenanceCost).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Origin).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Size).HasMaxLength(50);
+            entity.Property(e => e.Species).HasMaxLength(100);
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.Type).HasMaxLength(50);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.CreatedByNavigation).WithMany(p => p.AnimalCreatedByNavigations)
+                .HasForeignKey(d => d.CreatedBy)
+                .HasConstraintName("FK__Animals__Created__59063A47");
+
+            entity.HasOne(d => d.ModifiedByNavigation).WithMany(p => p.AnimalModifiedByNavigations)
+                .HasForeignKey(d => d.ModifiedBy)
+                .HasConstraintName("FK__Animals__Modifie__59FA5E80");
+        });
+
+        modelBuilder.Entity<AnimalImage>(entity =>
+        {
+            entity.HasKey(e => e.AimageId).HasName("PK__AnimalIm__95341BED5B9BE48D");
+
+            entity.Property(e => e.AimageId).HasColumnName("AImageID");
+            entity.Property(e => e.AnimalId).HasColumnName("AnimalID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(255)
+                .HasColumnName("ImageURL");
+
+            entity.HasOne(d => d.Animal).WithMany(p => p.AnimalImages)
+                .HasForeignKey(d => d.AnimalId)
+                .HasConstraintName("FK__AnimalIma__Anima__5CD6CB2B");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Category__19093A2BEF35629F");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Consignment>(entity =>
+        {
+            entity.HasKey(e => e.ConsignmentId).HasName("PK__Consignm__2AB758630C05E0BB");
+
+            entity.ToTable("Consignment");
+
+            entity.Property(e => e.ConsignmentId).HasColumnName("ConsignmentID");
+            entity.Property(e => e.AnimalId).HasColumnName("AnimalID");
+            entity.Property(e => e.CommissionRate).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.ConsignmentType).HasMaxLength(50);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.Notes).HasMaxLength(255);
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Animal).WithMany(p => p.Consignments)
+                .HasForeignKey(d => d.AnimalId)
+                .HasConstraintName("FK__Consignme__Anima__60A75C0F");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Consignments)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK__Consignme__Custo__5FB337D6");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.Consignments)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__Consignme__Order__628FA481");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__Customer__A4AE64B869B24628");
+
+            entity.ToTable("Customer");
+
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.LoyaltyLevel).HasMaxLength(50);
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.Points).HasDefaultValue(0);
+            entity.Property(e => e.ProfileUrl)
+                .HasMaxLength(255)
+                .HasColumnName("ProfileURL");
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__Order__C3905BAF5A94E879");
+
+            entity.ToTable("Order");
+
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.DeliveryMethod).HasMaxLength(50);
+            entity.Property(e => e.OrderDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.PaymentStatus).HasMaxLength(50);
+            entity.Property(e => e.PromotionId).HasColumnName("PromotionID");
+            entity.Property(e => e.ShippingAddress).HasMaxLength(255);
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.TotalAmount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.TotalAmountVat)
+                .HasColumnType("decimal(18, 2)")
+                .HasColumnName("TotalAmountVAT");
+            entity.Property(e => e.Vat)
+                .HasColumnType("decimal(5, 2)")
+                .HasColumnName("VAT");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK__Order__CustomerI__49C3F6B7");
+
+            entity.HasOne(d => d.Promotion).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.PromotionId)
+                .HasConstraintName("FK__Order__Promotion__4BAC3F29");
+        });
+
+        modelBuilder.Entity<OrderDetail>(entity =>
+        {
+            entity.HasKey(e => e.OrderDetailId).HasName("PK__OrderDet__D3B9D30CD61ACC9F");
+
+            entity.Property(e => e.OrderDetailId).HasColumnName("OrderDetailID");
+            entity.Property(e => e.Amount).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.AnimalId).HasColumnName("AnimalID");
+            entity.Property(e => e.Discount).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Subtotal).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Animal).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.AnimalId)
+                .HasConstraintName("FK__OrderDeta__Anima__6754599E");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__OrderDeta__Order__656C112C");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__OrderDeta__Produ__66603565");
+        });
+
+        modelBuilder.Entity<OrderPromotion>(entity =>
+        {
+            entity.HasKey(e => e.OrderPromotionId).HasName("PK__OrderPro__CE84A0E14B8E0B13");
+
+            entity.ToTable("OrderPromotion");
+
+            entity.Property(e => e.OrderPromotionId).HasColumnName("OrderPromotionID");
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.PromotionId).HasColumnName("PromotionID");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderPromotions)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__OrderProm__Order__4F7CD00D");
+
+            entity.HasOne(d => d.Promotion).WithMany(p => p.OrderPromotions)
+                .HasForeignKey(d => d.PromotionId)
+                .HasConstraintName("FK__OrderProm__Promo__4E88ABD4");
+        });
+
+        modelBuilder.Entity<Payment>(entity =>
+        {
+            entity.HasKey(e => e.PaymentId).HasName("PK__Payment__9B556A582359518A");
+
+            entity.ToTable("Payment");
+
+            entity.Property(e => e.PaymentId).HasColumnName("PaymentID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.CustomerId).HasColumnName("CustomerID");
+            entity.Property(e => e.Method).HasMaxLength(50);
+            entity.Property(e => e.OrderId).HasColumnName("OrderID");
+            entity.Property(e => e.PaymentDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.TransactionId)
+                .HasMaxLength(100)
+                .HasColumnName("TransactionID");
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Payments)
+                .HasForeignKey(d => d.CustomerId)
+                .HasConstraintName("FK__Payment__Custome__534D60F1");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.Payments)
+                .HasForeignKey(d => d.OrderId)
+                .HasConstraintName("FK__Payment__OrderID__52593CB8");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6ED8F9691BA");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.Brand).HasMaxLength(100);
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Discount).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Weight).HasColumnType("decimal(18, 2)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__3F466844");
+
+            entity.HasOne(d => d.CreatedByNavigation).WithMany(p => p.ProductCreatedByNavigations)
+                .HasForeignKey(d => d.CreatedBy)
+                .HasConstraintName("FK__Products__Create__412EB0B6");
+
+            entity.HasOne(d => d.ModifiedByNavigation).WithMany(p => p.ProductModifiedByNavigations)
+                .HasForeignKey(d => d.ModifiedBy)
+                .HasConstraintName("FK__Products__Modifi__4222D4EF");
+        });
+
+        modelBuilder.Entity<ProductImage>(entity =>
+        {
+            entity.HasKey(e => e.PimageId).HasName("PK__ProductI__218DDF19450FF604");
+
+            entity.Property(e => e.PimageId).HasColumnName("PImageID");
+            entity.Property(e => e.ImageUrl)
+                .HasMaxLength(255)
+                .HasColumnName("ImageURL");
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.ProductImages)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("FK__ProductIm__Produ__44FF419A");
+        });
+
+        modelBuilder.Entity<Promotion>(entity =>
+        {
+            entity.HasKey(e => e.PromotionId).HasName("PK__Promotio__52C42F2F281E1E52");
+
+            entity.ToTable("Promotion");
+
+            entity.Property(e => e.PromotionId).HasColumnName("PromotionID");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.DiscountPercent).HasColumnType("decimal(5, 2)");
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.Title).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Staff>(entity =>
+        {
+            entity.HasKey(e => e.StaffId).HasName("PK__Staff__96D4AAF77494851F");
+
+            entity.Property(e => e.StaffId).HasColumnName("StaffID");
+            entity.Property(e => e.Address).HasMaxLength(255);
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Phone).HasMaxLength(20);
+            entity.Property(e => e.Role).HasMaxLength(50);
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.UpdatedAt).HasColumnType("datetime");
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ koi-farm-application.sql
@@ -0,0 +1,178 @@
+-- Create Staff table
+CREATE DATABASE FA24_SE1716_PRN231_G1_KOIFARMSHOP
+
+USE FA24_SE1716_PRN231_G1_KOIFARMSHOP
+
+CREATE TABLE Staff (
+    StaffID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(255) NOT NULL,
+    FullName NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100),
+    Phone NVARCHAR(20) NOT NULL,
+    Address NVARCHAR(255),
+    Role NVARCHAR(50) NOT NULL, -- Example: Manager, Salesperson, Technician
+    Status NVARCHAR(50), -- Example: Active, Inactive
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+);
+
+-- Create Customer table
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY IDENTITY(1,1),
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(255) NOT NULL,
+    Name NVARCHAR(100) NOT NULL,
+    Phone NVARCHAR(20) NOT NULL,
+    Address NVARCHAR(255),
+    Points INT DEFAULT 0,
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+    ProfileURL NVARCHAR(255),
+    LoyaltyLevel NVARCHAR(50),
+	Status VARCHAR (50)
+);
+
+-- Create Category table
+CREATE TABLE Category (
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255),
+	Status VARCHAR (50)
+);
+
+-- Create Products table
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255),
+    Price DECIMAL(18, 2) NOT NULL,
+    StockQuantity INT NOT NULL,
+    Brand NVARCHAR(100),
+    Weight DECIMAL(18, 2),
+    Discount DECIMAL(5, 2),
+    BulkDiscount DECIMAL(5, 2),
+    ExpiryDate DATE,
+    ManufacturingDate DATE,
+    CategoryID INT FOREIGN KEY REFERENCES Category(CategoryID),
+    Status NVARCHAR(50),
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+	CreatedBy INT FOREIGN KEY REFERENCES Staff(StaffID),
+	ModifiedBy INT FOREIGN KEY REFERENCES Staff(StaffID)
+);
+
+-- Create ProductImages table
+CREATE TABLE ProductImages (
+    PImageID INT PRIMARY KEY IDENTITY(1,1),
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+    ImageURL NVARCHAR(255) NOT NULL
+);
+
+-- Create Promotion table
+CREATE TABLE Promotion (
+    PromotionID INT PRIMARY KEY IDENTITY(1,1),
+    Title NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255),
+    StartDate DATE,
+    EndDate DATE,
+    DiscountPercent DECIMAL(5, 2),
+    Status NVARCHAR(50)
+);
+
+-- Create Order table
+CREATE TABLE [Order] (
+    OrderID INT PRIMARY KEY IDENTITY(1,1),
+    CustomerID INT FOREIGN KEY REFERENCES Customer(CustomerID),
+    OrderDate DATETIME DEFAULT GETDATE(),
+    TotalAmount DECIMAL(18, 2) NOT NULL,
+    PromotionID INT FOREIGN KEY REFERENCES Promotion(PromotionID),
+    ShippingAddress NVARCHAR(255),
+    DeliveryMethod NVARCHAR(50),
+    PaymentStatus NVARCHAR(50),
+    VAT DECIMAL(5, 2),
+    TotalAmountVAT DECIMAL(18, 2),
+	Status NVARCHAR(50)
+);
+
+-- Create OrderPromotion table
+CREATE TABLE OrderPromotion (
+    OrderPromotionID INT PRIMARY KEY IDENTITY(1,1),
+    PromotionID INT FOREIGN KEY REFERENCES Promotion(PromotionID),
+    OrderID INT FOREIGN KEY REFERENCES [Order](OrderID)
+);
+
+-- Create Payment table
+CREATE TABLE Payment (
+    PaymentID INT PRIMARY KEY IDENTITY(1,1),
+    OrderID INT FOREIGN KEY REFERENCES [Order](OrderID),
+    CustomerID INT FOREIGN KEY REFERENCES Customer(CustomerID),
+    Method NVARCHAR(50),
+    Status NVARCHAR(50),
+    TransactionID NVARCHAR(100),
+    PaymentDate DATETIME DEFAULT GETDATE(),
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME
+);
+
+-- Create Animals table
+CREATE TABLE Animals (
+    AnimalID INT PRIMARY KEY IDENTITY(1,1),
+    Origin NVARCHAR(100),
+    Species NVARCHAR(100),
+    Type NVARCHAR(50),
+    Gender NVARCHAR(10),
+    Size NVARCHAR(50),
+    Certificate NVARCHAR(255),
+    Price DECIMAL(18, 2),
+    Status NVARCHAR(50),
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+    MaintenanceCost DECIMAL(18, 2),
+    Color NVARCHAR(50),
+    AmountFeed DECIMAL(18, 2),
+    HealthStatus NVARCHAR(255),
+    FarmOrigin NVARCHAR(100),
+	BirthYear INT,
+    Description NVARCHAR(255),
+	CreatedBy INT FOREIGN KEY REFERENCES Staff(StaffID),
+	ModifiedBy INT FOREIGN KEY REFERENCES Staff(StaffID)
+);
+
+-- Create AnimalImages table
+CREATE TABLE AnimalImages (
+    AImageID INT PRIMARY KEY IDENTITY(1,1),
+    AnimalID INT FOREIGN KEY REFERENCES Animals(AnimalID),
+    ImageURL NVARCHAR(255) NOT NULL
+);
+
+-- Create Consignment table
+CREATE TABLE Consignment (
+    ConsignmentID INT PRIMARY KEY IDENTITY(1,1),
+    CustomerID INT FOREIGN KEY REFERENCES Customer(CustomerID),
+    AnimalID INT FOREIGN KEY REFERENCES Animals(AnimalID),
+    ConsignmentType NVARCHAR(50), -- Enum: Online, Offline
+    StartDate DATE,
+    EndDate DATE,
+    Price DECIMAL(18, 2),
+    Status NVARCHAR(50),
+    CreatedAt DATETIME DEFAULT GETDATE(),
+    UpdatedAt DATETIME,
+    Notes NVARCHAR(255),
+    CommissionRate DECIMAL(5, 2),
+    OrderID INT FOREIGN KEY REFERENCES [Order](OrderID)
+);
+
+
+-- Create OrderDetails table
+CREATE TABLE OrderDetails (
+    OrderDetailID INT PRIMARY KEY IDENTITY(1,1),
+    OrderID INT FOREIGN KEY REFERENCES [Order](OrderID),
+    ProductID INT FOREIGN KEY REFERENCES Products(ProductID),
+	AnimalID INT FOREIGN KEY REFERENCES Animals(AnimalID),
+    Quantity INT NOT NULL,
+    Price DECIMAL(18, 2) NOT NULL,
+    Amount DECIMAL(18, 2),
+    Subtotal DECIMAL(18, 2),
+    Discount DECIMAL(5, 2)
+);
