9006dccb4c5ca234a3c6df361ec5c27b8f8a81b6
Nigo46 <nigo4666@gmail.com>
2024-10-02 10:51:55 +0700

    [feat] Compare Fish

--- FA24_SE1716_PRN231_G1_KOIFARMSHOP/KOIFARMSHOP.MVCWebApp/Controllers/AnimalsController.cs
+++ FA24_SE1716_PRN231_G1_KOIFARMSHOP/KOIFARMSHOP.MVCWebApp/Controllers/AnimalsController.cs
@@ -29,18 +29,21 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
             using (var httpClient = new HttpClient())
             {
                 using (var respone = await httpClient.GetAsync(Const.APIEndPoint + "Animals"))
+                {
                 { 
                     if (respone.IsSuccessStatusCode)
                     {
                         var content = await respone.Content.ReadAsStringAsync();
                         var result = JsonConvert.DeserializeObject<BusinessResult>(content);
 
+                        if (result != null && result.Data != null)
                         if(result != null && result.Data != null)
                         {
                             var data = JsonConvert.DeserializeObject<List<Animal>>(result.Data.ToString());
                             return View(data);
                         }
                     }
+
             
                 }
             }
@@ -89,11 +92,17 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
         }
 
         // GET: Animals/Create
+        public async Task<IActionResult> Create()
+        {
+            var animals = new List<Animal>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var respone = await httpClient.GetAsync(Const.APIEndPoint + "Animals"))
         public async  Task<IActionResult> Create()
         {
-            ViewData["CreatedBy"] = new SelectList(_context.Staff, "StaffId", "FullName");
+                ViewData["CreatedBy"] = new SelectList(_context.Staff, "StaffId", "FullName");
                 ViewData["ModifiedBy"] = new SelectList(_context.Staff, "StaffId", "FullName");
-               return View();
+                return View();
         }
 
 
@@ -126,6 +135,36 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
                         var content = await respone.Content.ReadAsStringAsync();
                         var result = JsonConvert.DeserializeObject<BusinessResult>(content);
 
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<List<Animal>>(result.Data.ToString());
+                            return View(data);
+                        }
+                    }
+
+                }
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Staff, "StaffId", "FullName");
+            ViewData["ModifiedBy"] = new SelectList(_context.Staff, "StaffId", "FullName");
+
+            return View(new List<Animal>());
+        }
+
+        // POST: Animals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AnimalId,Origin,Species,Type,Gender,Size,Certificate,Price,Status,CreatedAt,UpdatedAt,MaintenanceCost,Color,AmountFeed,HealthStatus,FarmOrigin,BirthYear,Description,CreatedBy,ModifiedBy")] Animal animal)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(animal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
                         if (result != null && result.Status == Const.SUCCESS_CREATE_CODE)
                         {
                             savaStatus = true;
@@ -185,6 +224,23 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
             return View(animal);
         }
 
+        // GET: Animals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Staff, "StaffId", "FullName", animal.CreatedBy);
+            ViewData["ModifiedBy"] = new SelectList(_context.Staff, "StaffId", "FullName", animal.ModifiedBy);
+            return View(animal);
+        }
 
         // POST: Animals/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
@@ -193,6 +249,53 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("AnimalId,Origin,Species,Type,Gender,Size,Certificate,Price,Status,CreatedAt,UpdatedAt,MaintenanceCost,Color,AmountFeed,HealthStatus,FarmOrigin,BirthYear,Description,CreatedBy,ModifiedBy")] Animal animal)
         {
+            if (id != animal.AnimalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(animal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AnimalExists(animal.AnimalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatedBy"] = new SelectList(_context.Staff, "StaffId", "FullName", animal.CreatedBy);
+            ViewData["ModifiedBy"] = new SelectList(_context.Staff, "StaffId", "FullName", animal.ModifiedBy);
+            return View(animal);
+        }
+
+        // GET: Animals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var animal = await _context.Animals
+                .Include(a => a.CreatedByNavigation)
+                .Include(a => a.ModifiedByNavigation)
+                .FirstOrDefaultAsync(m => m.AnimalId == id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
             
 
             bool updateStatus = false;
@@ -260,6 +363,14 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal != null)
+            {
+                _context.Animals.Remove(animal);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
             bool deleteStatus = false;
 
             using (var httpClient = new HttpClient())
@@ -297,6 +408,7 @@ namespace KOIFARMSHOP.MVCWebApp.Controllers
         {
             return _context.Animals.Any(e => e.AnimalId == id);
         }
-
+        
     }
 }
+
--- FA24_SE1716_PRN231_G1_KOIFARMSHOP/KOIFARMSHOP.Service/Services/AnimalService.cs
+++ FA24_SE1716_PRN231_G1_KOIFARMSHOP/KOIFARMSHOP.Service/Services/AnimalService.cs
@@ -3,14 +3,18 @@ using KOIFARMSHOP.Data;
 using KOIFARMSHOP.Common;
 using KOIFARMSHOP.Data.Models;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 namespace KOIFARMSHOP.Service.Services
 {
     public interface IAnimalService
     {
         Task<IBusinessResult> GetAll();
-        Task<IBusinessResult> GetByID(int id);
+        Task<IBusinessResult> GetByID(string id);
         Task<IBusinessResult> Save(Animal animal);
         Task<IBusinessResult> DeleteByID(int id);
+        Task<IBusinessResult> CompareMultipleKoiFishPrices(List<int> koiFishIds);
+
+
     }
     public class AnimalService : IAnimalService
     {
@@ -19,22 +23,23 @@ namespace KOIFARMSHOP.Service.Services
         {
             _unitOfWork ??= new UnitOfWork();
         }
-        public async Task<IBusinessResult> GetAll() {
+        public async Task<IBusinessResult> GetAll()
+        {
             var list = await _unitOfWork.AnimalRepository.GetAllAsync();
             if (list == null)
             {
-                return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG,new List<Animal>());
+                return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG, new List<Animal>());
             }
             else
             {
                 return new BusinessResult(Const.SUCCESS_CREATE_CODE, Const.SUCCESS_CREATE_MSG, list);
             }
         }
-        public async Task<IBusinessResult> GetByID(int id)
+        public async Task<IBusinessResult> GetByID(string id)
         {
             #region Business rule
             #endregion
-            var list = await _unitOfWork.AnimalRepository.GetByIdAsync(id);
+            var list = _unitOfWork.AnimalImageRepository.GetByIdAsync(id);
             if (list == null)
             {
                 return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG, new List<Animal>());
@@ -50,7 +55,7 @@ namespace KOIFARMSHOP.Service.Services
             try
             {
                 int result = -1;
-                var animalTmp = await _unitOfWork.AnimalRepository.GetByIdAsync(animal.AnimalId);
+                var animalTmp = _unitOfWork.AnimalRepository.GetByIdAsync(animal.AnimalId);
                 if (animalTmp != null)
                 {
                     #region Business Rule
@@ -60,7 +65,7 @@ namespace KOIFARMSHOP.Service.Services
 
                     if (result > 0)
                     {
-                        return new BusinessResult(Const.SUCCESS_UPDATE_CODE, Const.SUCCESS_UPDATE_MSG, new List<Animal>());
+                        return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG, new List<Animal>());
                     }
                     else
                     {
@@ -87,14 +92,16 @@ namespace KOIFARMSHOP.Service.Services
             }
         }
 
-        public async Task<IBusinessResult> DeleteByID(int id)
+        public async Task<IBusinessResult> DeleteByID(string id)
         {
             try
             {
                 var animalById = await _unitOfWork.AnimalRepository.GetByIdAsync(id);
-                if (animalById == null) {
+                if (animalById == null)
+                {
                     return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG, new Animal());
-                }else
+                }
+                else
                 {
                     var result = await _unitOfWork.AnimalRepository.RemoveAsync(animalById);
                     if (result)
@@ -106,8 +113,68 @@ namespace KOIFARMSHOP.Service.Services
                         return new BusinessResult(Const.FAIL_CREATE_CODE, Const.FAIL_CREATE_MSG, animalById);
                     }
                 }
-            }catch (Exception ex) { return new BusinessResult(Const.ERROR_EXCEPTION, ex.ToString()); }
+            }
+            catch (Exception ex) { return new BusinessResult(Const.ERROR_EXCEPTION, ex.ToString()); }
         }
 
+        public async Task<IBusinessResult> CompareMultipleKoiFishPrices(List<int> koiFishIds)
+        {
+            try
+            {
+                // Fetch koi fish details for all provided IDs
+                var koiFishList = new List<Animal>();
+                foreach (var koiFishId in koiFishIds)
+                {
+                    var koiFish = await _unitOfWork.AnimalRepository.GetByIdAsync(koiFishId);
+                    if (koiFish != null)
+                    {
+                        koiFishList.Add(koiFish);
+                    }
+                }
+
+                // Check if we have enough data to compare
+                if (koiFishList.Count < 2)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA_CODE, "At least two koi fish are required for comparison.", koiFishList);
+                }
+
+                // Check if we have enough data to compare
+                if (koiFishList.Count < 2)
+                {
+                    return new BusinessResult(Const.WARNING_NO_DATA_CODE, "At least two koi fish are required for comparison.", koiFishList);
+                }
+
+                // Sort the koi fish by price in ascending order
+                koiFishList = koiFishList.OrderBy(k => k.Price).ToList();
+
+                // Create a comparison message
+                string comparisonMessage = "Comparison of Koi Fish Prices:\n";
+                for (int i = 0; i < koiFishList.Count - 1; i++)
+                {
+                    var currentKoiFish = koiFishList[i];
+                    var nextKoiFish = koiFishList[i + 1];
+                    if (currentKoiFish.Price < nextKoiFish.Price)
+                    {
+                        comparisonMessage += $"Koi fish with ID {nextKoiFish.AnimalId} ({nextKoiFish.Species}) is more expensive than koi fish with ID {currentKoiFish.AnimalId} ({currentKoiFish.Species}).\n";
+                    }
+                    else if (currentKoiFish.Price == nextKoiFish.Price)
+                    {
+                        comparisonMessage += $"Koi fish with ID {nextKoiFish.AnimalId} ({nextKoiFish.Species}) has the same price as koi fish with ID {currentKoiFish.AnimalId} ({currentKoiFish.Species}).\n";
+                    }
+                }
+
+                // Return the list of koi fish with comparison message
+                var result = new
+                {
+                    KoiFishList = koiFishList,
+                    ComparisonMessage = comparisonMessage
+                };
+                return new BusinessResult(Const.SUCCESS_CREATE_CODE, Const.SUCCESS_CREATE_MSG, result);
+            }
+            catch (Exception ex)
+            {
+                return new BusinessResult(Const.ERROR_EXCEPTION, ex.ToString());
+            }
+        }
     }
 }
